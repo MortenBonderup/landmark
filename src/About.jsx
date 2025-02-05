2cbf03ab2b015caf7067db6ec0c38ddbbadadff4
Morten Bonderup <moab@eaaa.dk>
2025-02-05 14:42:40 +0100

    Version4

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,7 @@ import { Paper, Typography } from '@mui/material';
 
 function About() {
     return (
-        <Paper sx={{ padding: '16px', marginTop: '16px' }}>
+        <Paper sx={{ padding: '16px', marginTop: '16px', marginBottom: '25px' }}>
             <Typography variant="h4">About</Typography>
             <Typography variant="body1">
                 This website provides detailed information about famous landmarks around the world. It was created to help people learn more about these amazing places and their histories.
